3182857bd33402e9b671f4c6d254eae63488342e
diogoaraujo <araujo@SURFDEV.localdomain>
2022-04-19 10:32:20 -0300

    testando request

--- webscraping.py
+++ webscraping.py
@@ -1,5 +1,7 @@
 import requests
-<<<<<<< HEAD
-=======
-test
->>>>>>> ffe03c418f82ad1f946c1d2636d6e464a3db6d61
+
+response = requests.get('https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204')
+
+print ('Status Code' , response.status_code)
+print ('Header')
+print (response.headers)
