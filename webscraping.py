95d4ed6ef014efa72cf551bdadf1aca07645835b
diogoaraujo <diogorjbr@gmail.com>
2022-05-17 20:16:55 -0300

    testa BeautifulSoup

--- webscraping.py
+++ webscraping.py
@@ -1,11 +1,16 @@
 import requests
-
-response = requests.get('https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204')
+from bs4 import BeautifulSoup
+response = requests.get('https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204')
+content = response.content
 
 print ('Status Code' , response.status_code)
 print ('Header')
-
 print (response.headers)
 
 print('\n  Content')
 print(response.content)
+
+site = BeautifulSoup(content, 'html.parser')
+
+calendario=site.find('div',attrs={'class': 'input-group'})
+print(calendario)
