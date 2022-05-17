37542d1a99bec325225bda35db87e1223e329be4
diogoaraujo <diogorjbr@gmail.com>
2022-05-17 17:44:33 -0300

    testando request sucupira

--- webscraping.py
+++ webscraping.py
@@ -4,4 +4,8 @@ response = requests.get('https://sucupira.capes.gov.br/sucupira/public/consultas
 
 print ('Status Code' , response.status_code)
 print ('Header')
+
 print (response.headers)
+
+print('\n  Content')
+print(response.content)
