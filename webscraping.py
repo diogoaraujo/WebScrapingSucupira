0338d5a4d98f958adc8cd39321955092cb5c7b18
diogoaraujo <diogorjbr@gmail.com>
2022-05-17 23:35:20 -0300

    Retorna Form Dados do Envio

--- webscraping.py
+++ webscraping.py
@@ -3,14 +3,23 @@ from bs4 import BeautifulSoup
 response = requests.get('https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/envioColeta/dadosFotoEnvioColeta.jsf;jsessionid=HTARxpwFZdk8JeCHc7Mg7WCu.sucupira-204')
 content = response.content
 
-print ('Status Code' , response.status_code)
+''' print ('Status Code' , response.status_code)
 print ('Header')
-print (response.headers)
+print (response.headers) '''
 
-print('\n  Content')
-print(response.content)
+#print('\n  Content')
+#print(response.content)
 
 site = BeautifulSoup(content, 'html.parser')
 
-calendario=site.find('div',attrs={'class': 'input-group'})
-print(calendario)
+#Dados do Envio
+DadosDoEnvio=site.find('div',attrs={'class': 'conteudo'})
+#Calendario
+Calendario=DadosDoEnvio.find('div', attrs={'class': 'input-group'})
+#Instituição de Ensino Superior
+# Instituicao= DadosDoEnvio.find()
+
+# print(DadosDoEnvio.find())
+print("-------------------------------")
+print(DadosDoEnvio.find())
+
